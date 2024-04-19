d6fa13d23dabf0f6d09dd02781463e25de543241
Maciej Krawczyk <kontakt.maciejkrawczyk@gmail.com>
2024-04-19 19:18:36 +0200

    ok

--- frontend-rrr-robot/src/App.vue
+++ frontend-rrr-robot/src/App.vue
@@ -5,230 +5,274 @@
 
   <div v-else class="min-h-screen bg-gray-100 flex flex-col items-center justify-start pt-8">
     <div class="w-full max-w-6xl px-4 md:px-8 lg:px-12">
+      <Drawer>
 
-      <h1>Robot Controller</h1>
+        <h1>Robot Controller</h1>
 
-      <Tabs default-value="manual" class="w-full">
+        <Tabs default-value="manual" class="w-full">
 
-        <TabsContent value="manual" class="overflow-auto h-[70vh]">
+          <TabsContent value="manual" class="overflow-auto h-[70vh]">
 
-          <h2 class="font-bold text-4xl pt-4 pb-4">Manual</h2>
+            <h2 class="font-bold text-4xl pt-4 pb-4">Manual</h2>
 
-          <div class="flex flex-col gap-2">
-            <div class="flex gap-1">
-              <Button
-                  @mousedown="onTheta1Plus"
-                  @mouseup="onTheta1Stop"
-              >
-                Theta1+
-              </Button>
-              <Button
-                  @mousedown="onTheta1Minus"
-                  @mouseup="onTheta1Stop"
-              >
-                Theta1-
-              </Button>
+            <div class="flex flex-col gap-2">
+              <div class="flex gap-1">
+                <Button
+                    @mousedown="onTheta1Plus"
+                    @mouseup="onTheta1Stop"
+                >
+                  Theta1+
+                </Button>
+                <Button
+                    @mousedown="onTheta1Minus"
+                    @mouseup="onTheta1Stop"
+                >
+                  Theta1-
+                </Button>
+              </div>
+
+              <div class="flex gap-1">
+                <Button
+                    @mousedown="onTheta3Plus"
+                    @mouseup="onTheta3Stop"
+                >
+                  Theta3+
+                </Button>
+                <Button
+                    @mousedown="onTheta3Minus"
+                    @mouseup="onTheta3Stop"
+                >
+                  Theta3-
+                </Button>
+              </div>
             </div>
 
-            <div class="flex gap-1">
-              <Button
-                  @mousedown="onTheta3Plus"
-                  @mouseup="onTheta3Stop"
+
+          </TabsContent>
+
+          <TabsContent value="code" class="overflow-auto h-[70vh]">
+
+            <h2 class="font-bold text-4xl pt-4 pb-4">Program</h2>
+
+            <Form v-slot="{setValues}" @submit="(values) => saveCodeToDB(values)">
+              <div
+                  v-for="(command, index) in code"
+                  :key="command.id"
+                  class="w-full flex justify-around items-center gap-3 m-1.5"
               >
-                Theta3+
+
+                <FormField class="w-10/12" v-slot="{componentField}" :name="`name-${command.id}`">
+                  <FormItem class="w-full">
+                    <Select class="w-full" v-model="command.name" v-bind="componentField" :default-value="command.name">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select command"/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Commands</SelectLabel>
+                          <SelectItem value="move_to">
+                            move_to
+                          </SelectItem>
+                          <SelectItem value="sleep">
+                            sleep
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                  </FormItem>
+                </FormField>
+
+                <FormField class="w-10/12" v-if="command.name === 'move_to'" v-slot="{componentField}"
+                           :name="`body-${command.id}`">
+                  <FormItem class="w-full">
+
+                    <Select class="w-full" v-bind="componentField" :default-value="command.body">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select position"/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Positions</SelectLabel>
+                          <SelectItem v-for="position in positions" :value="String(position.id)">
+                            {{ position.id }}
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                  </FormItem>
+                </FormField>
+
+
+                <FormField class="w-10/12" v-if="command.name === 'sleep'" v-slot="{componentField}"
+                           :name="`body-${command.id}`">
+                  <FormItem class="w-full">
+                    <Select class="w-full" v-bind="componentField" :default-value="command.body">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select time"/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Time</SelectLabel>
+                          <SelectItem value="1">1s</SelectItem>
+                          <SelectItem value="2">2s</SelectItem>
+                          <SelectItem value="3">3s</SelectItem>
+                          <SelectItem value="4">4s</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                </FormField>
+
+                <Button variant="destructive" @click.prevent="removeCommand(command.id)">
+                  <Trash2/>
+                </Button>
+              </div>
+
+              <Button type="submit" class="rounded-full w-[80px] h-[80px] mt-5" variant="outline">
+                <Save/>
               </Button>
-              <Button
-                  @mousedown="onTheta3Minus"
-                  @mouseup="onTheta3Stop"
-              >
-                Theta3-
+
+              <Button class="mt-5 mb-5 rounded-full w-[80px] h-[80px] ml-5" @click.prevent="addCommand"
+                      variant="outline">
+                <Plus/>
               </Button>
+
+            </Form>
+
+            <DrawerTrigger as-child>
+              <Button class="mt-5 mb-5 rounded-full w-[80px] h-[80px]">
+                <Play/>
+              </Button>
+            </DrawerTrigger>
+
+          </TabsContent>
+
+          <TabsContent value="db" class="overflow-auto h-[70vh]">
+
+            <h2 class="font-bold text-4xl pt-4 pb-4">Database</h2>
+
+            <Table>
+              <TableCaption>A list of saved positions</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>id</TableHead>
+                  <TableHead>theta1</TableHead>
+                  <TableHead>theta2</TableHead>
+                  <TableHead>theta3</TableHead>
+                  <TableHead>x</TableHead>
+                  <TableHead>y</TableHead>
+                  <TableHead>z</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow v-for="position in positions" :key="position.id">
+                  <TableCell>{{ position.id }}</TableCell>
+                  <TableCell>{{ position.theta1 }}</TableCell>
+                  <TableCell>{{ position.theta2 }}</TableCell>
+                  <TableCell>{{ position.theta3 }}</TableCell>
+                  <TableCell>{{ position.x }}</TableCell>
+                  <TableCell>{{ position.y }}</TableCell>
+                  <TableCell>{{ position.z }}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+
+          </TabsContent>
+
+          <TabsList class="w-full flex justify-around items-center">
+            <TabsTrigger value="manual" class="w-1/3">
+              JOG
+            </TabsTrigger>
+            <TabsTrigger value="code" class="w-1/3">
+              CODE
+            </TabsTrigger>
+            <TabsTrigger value="db" class="w-1/3">
+              DB
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+
+        <footer
+            class="ml-auto mr-auto  bottom-3 w-auto z-10 flex justify-around border flex-col mt-5 p-2 bg-white shadow-lg">
+          <div class="w-full flex justify-around">
+            <div class="w-1/6">
+              <div class="w-full text-center">theta1</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ theta1 }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+            <div class="w-1/6">
+              <div class="w-full text-center">theta2</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ theta2 }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+            <div class="w-1/6">
+              <div class="w-full text-center">theta3</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ theta3 }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+            <div class="w-1/6">
+              <div class="w-full text-center">x</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ x }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+            <div class="w-1/6">
+              <div class="w-full text-center">y</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ y }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+            <div class="w-1/6">
+              <div class="w-full text-center">z</div>
+              <div class="w-full h-full text-center" v-if="isConnected">{{ z }}</div>
+              <Skeleton v-else class="w-full h-[24px]"/>
             </div>
           </div>
 
-
-        </TabsContent>
-
-        <TabsContent value="code" class="overflow-auto h-[70vh]">
-
-          <h2 class="font-bold text-4xl pt-4 pb-4">Program</h2>
-
-          <Form v-slot="{setValues}" @submit="(values) => saveCodeToDB(values)">
-            <div
-                v-for="(command, index) in code"
-                :key="command.id"
-                class="w-full flex justify-around items-center gap-3 m-1.5"
-            >
-
-              <FormField class="w-10/12" v-slot="{componentField}" :name="`name-${command.id}`">
-                <FormItem class="w-full">
-                  <Select class="w-full" v-model="command.name" v-bind="componentField" :default-value="command.name">
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select command"/>
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Commands</SelectLabel>
-                        <SelectItem value="move_to">
-                          move_to
-                        </SelectItem>
-                        <SelectItem value="sleep">
-                          sleep
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-
-                </FormItem>
-              </FormField>
-
-              <FormField class="w-10/12" v-if="command.name === 'move_to'" v-slot="{componentField}"
-                         :name="`body-${command.id}`">
-                <FormItem class="w-full">
-
-                  <Select class="w-full" v-bind="componentField" :default-value="command.body">
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select position"/>
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Positions</SelectLabel>
-                        <SelectItem v-for="position in positions" :value="String(position.id)">
-                          {{ position.id }}
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-
-                </FormItem>
-              </FormField>
-
-
-              <FormField class="w-10/12" v-if="command.name === 'sleep'" v-slot="{componentField}"
-                         :name="`body-${command.id}`">
-                <FormItem class="w-full">
-                  <Select class="w-full" v-bind="componentField" :default-value="command.body">
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select time"/>
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Time</SelectLabel>
-                        <SelectItem value="1">1s</SelectItem>
-                        <SelectItem value="2">2s</SelectItem>
-                        <SelectItem value="3">3s</SelectItem>
-                        <SelectItem value="4">4s</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              </FormField>
-
-              <Button variant="destructive" @click.prevent="removeCommand(command.id)">
-                <Trash2/>
-              </Button>
-            </div>
-
-            <Button type="submit" class="rounded-full w-[80px] h-[80px] mt-5" variant="outline">
-              <Save/>
-            </Button>
-
-            <Button class="mt-5 mb-5 rounded-full w-[80px] h-[80px] ml-5" @click.prevent="addCommand" variant="outline">
-              <Plus/>
-            </Button>
-
-          </Form>
-
-          <Button class="mt-5 mb-5 rounded-full w-[80px] h-[80px]">
-            <Play/>
+          <Button class="w-full mt-3" @click.prevent="savePositionToDB({x,y,z,theta1,theta2,theta3})">
+            Save to database
           </Button>
+        </footer>
 
-        </TabsContent>
 
-        <TabsContent value="db" class="overflow-auto h-[70vh]">
+        <DrawerContent>
+          <div class="mx-auto w-full max-w-sm">
+            <DrawerHeader>
+              <DrawerTitle>Program Compilation</DrawerTitle>
+              <DrawerDescription>You will be able to start program shortly</DrawerDescription>
+            </DrawerHeader>
 
-          <h2 class="font-bold text-4xl pt-4 pb-4">Database</h2>
+            <div class="w-full flex flex-col gap-3 items-start">
 
-          <Table>
-            <TableCaption>A list of saved positions</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead>id</TableHead>
-                <TableHead>theta1</TableHead>
-                <TableHead>theta2</TableHead>
-                <TableHead>theta3</TableHead>
-                <TableHead>x</TableHead>
-                <TableHead>y</TableHead>
-                <TableHead>z</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow v-for="position in positions" :key="position.id">
-                <TableCell>{{ position.id }}</TableCell>
-                <TableCell>{{ position.theta1 }}</TableCell>
-                <TableCell>{{ position.theta2 }}</TableCell>
-                <TableCell>{{ position.theta3 }}</TableCell>
-                <TableCell>{{ x }}</TableCell>
-                <TableCell>{{ y }}</TableCell>
-                <TableCell>{{ z }}</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+              <div class="flex justify-center items-center font-bold">
+                <DoneSmall />
+                <p>Calculating points, cnt = 0.5</p>
+              </div>
 
-        </TabsContent>
+              <div class="flex justify-center items-center font-bold">
+                <LoadingSmall />
+                <p>Calculating trajectory, step = 0.01s</p>
+              </div>
 
-        <TabsList class="w-full flex justify-around items-center">
-          <TabsTrigger value="manual" class="w-1/3">
-            JOG
-          </TabsTrigger>
-          <TabsTrigger value="code" class="w-1/3">
-            CODE
-          </TabsTrigger>
-          <TabsTrigger value="db" class="w-1/3">
-            DB
-          </TabsTrigger>
-        </TabsList>
-      </Tabs>
+              <div class="flex justify-center items-center font-bold">
+                <LoadingSmall />
+                <p>Calculating velocities</p>
+              </div>
 
-      <footer
-          class="ml-auto mr-auto  bottom-3 w-auto z-10 flex justify-around border flex-col mt-5 p-2 bg-white shadow-lg">
-        <div class="w-full flex justify-around">
-          <div class="w-1/6">
-            <div class="w-full text-center">theta1</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ theta1 }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
+            </div>
+
+<!--            <DrawerFooter>-->
+<!--              <Button>Submit</Button>-->
+<!--              <DrawerClose as-child>-->
+<!--                <Button variant="outline">-->
+<!--                  Cancel-->
+<!--                </Button>-->
+<!--              </DrawerClose>-->
+<!--            </DrawerFooter>-->
           </div>
-          <div class="w-1/6">
-            <div class="w-full text-center">theta2</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ theta2 }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
-          </div>
-          <div class="w-1/6">
-            <div class="w-full text-center">theta3</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ theta3 }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
-          </div>
-          <div class="w-1/6">
-            <div class="w-full text-center">x</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ x }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
-          </div>
-          <div class="w-1/6">
-            <div class="w-full text-center">y</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ y }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
-          </div>
-          <div class="w-1/6">
-            <div class="w-full text-center">z</div>
-            <div class="w-full h-full text-center" v-if="isConnected">{{ z }}</div>
-            <Skeleton v-else class="w-full h-[24px]"/>
-          </div>
-        </div>
+        </DrawerContent>
 
-        <Button class="w-full mt-3" @click.prevent="savePositionToDB({x,y,z,theta1,theta2,theta3})">
-          Save to database
-        </Button>
-      </footer>
+      </Drawer>
 
     </div>
   </div>
@@ -257,6 +301,19 @@ import {Toaster} from "@/components/ui/toast"
 import {Command, Position} from "@/types.ts";
 import LoadingScreen from "@/components/LoadingScreen.vue";
 import {parseCommandsToCode} from "@/utils.ts";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
+import LoadingSmall from "@/components/LoadingSmall.vue";
+import DoneSmall from "@/components/DoneSmall.vue";
+
 
 const socket = io('http://localhost:5000');
 let serverResponse = ref({});
@@ -336,9 +393,6 @@ const saveCodeToDB = async (values: { [key: string]: string }) => {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    // const responseData = await response.json()
-    // code.value = JSON.parse(responseData.code)
-    // console.log(code.value)
     toast({
       title: 'Success',
       description: 'Program saved! Click RUN'
---
+++ frontend-rrr-robot/src/components/DoneSmall.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg fill="#000000" width="40px" height="40px" viewBox="-6 -7 35 35" id="d9090658-f907-4d85-8bc1-743b70378e93"
+       data-name="Livello 1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
+    <path id="70fa6808-131f-4233-9c3a-fc089fd0c1c4" data-name="done circle"
+          stroke="green"
+          fill="green"
+          d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0ZM11.52,17L6,12.79l1.83-2.37L11.14,13l4.51-5.08,2.24,2Z"/>
+  </svg>
+</template>
